1931dd2a8349d1bca08f2614d2c58331563c00e5
ddddyx <47801464+ddddyx@users.noreply.github.com>
2019-02-21 15:37:24 +0800

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,13 +19,13 @@ $(function() {
 $(document).ready(function() {
     // zoom in/zoom out animations
     if ($(".container").hasClass('fadeOut')) {
-        $(".container").removeClass("fadeOut").addClass("fadeIn2");
+        $(".container").removeClass("fadeOut").addClass("ffadeIn");
     }
     if ($(".wrapper").hasClass('fadeOut')) {
         $(".wrapper").removeClass("fadeOut").addClass("fadeIn");
     }
     $(".zoombtn").click(function() {
-        $(".container").removeClass("fadeIn").addClass("fadeOut");
+        $(".container").removeClass("ffadeIn").addClass("fadeOut");
         $(".wrapper").removeClass("fadeIn").addClass("fadeOut");
     });
     // go up button
